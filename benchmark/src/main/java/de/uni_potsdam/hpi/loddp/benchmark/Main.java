8603a8b70bd9b3c799b36f93a5f5d17e045d4fae
Benedikt Forchhammer <b.forchhammer@mind2.de>
2013-06-03 12:00:25 +0200

    First working version of benchmark program.

--- benchmark/src/main/java/de/uni_potsdam/hpi/loddp/benchmark/Main.java
+++ benchmark/src/main/java/de/uni_potsdam/hpi/loddp/benchmark/Main.java
@@ -1,19 +1,56 @@
 package de.uni_potsdam.hpi.loddp.benchmark;
 
-
-import org.apache.pig.PigRunner;
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.backend.executionengine.ExecJob;
 import org.apache.pig.tools.pigstats.PigStats;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
- * Main class containing main().
+ * Main class.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        String[] pig_args = new String[] {
-                "classes_by_entity.pig"
-        };
-        PigStats stats = PigRunner.run(pig_args, new PigProgressListener());
+    private PigServer pig;
+
+    protected Main() throws IOException {
+        this.pig = new PigServer(ExecType.MAPREDUCE);
+
+        // Register UDF + required libraries.
+        this.pig.registerJar("ldif-single-0.5.1-jar-with-dependencies.jar");
+        this.pig.registerJar("loddesc-core-0.1.jar");
+    }
+
+    protected void runScript() throws IOException {
+        if (this.pig.existsFile("results-topClassesByEntities")) {
+            this.pig.deleteFile("results-topClassesByEntities");
+            System.out.println("Previous output files deleted");
+        }
+        this.pig.registerScript(ClassLoader.getSystemClassLoader().getResourceAsStream("classes_by_entity.pig"));
+        ExecJob job = this.pig.store("topClassesByEntities", "results-topClassesByEntities");
+        PigStats stats = job.getStatistics();
+        System.out.println(String.format("Pig job took %dms.", stats.getDuration()));
+    }
+
+    protected void shutdown() {
+        if (this.pig != null) {
+            this.pig.shutdown();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println();
+        Main main = null;
+        try {
+            main = new Main();
+            main.runScript();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (main != null) main.shutdown();
+        }
     }
 
 }
--- benchmark/src/main/resources/classes_by_entity.pig
+++ benchmark/src/main/resources/classes_by_entity.pig
@@ -1,7 +1,4 @@
 -- Top Classes by Entity
-REGISTER /usr/share/java/scala-library.jar;
-REGISTER ldif-single-0.5.1-jar-with-dependencies.jar;
-REGISTER loddesc-core-0.1.jar;
 
 quads = LOAD 'file.nq'
 	USING de.wbsg.loddesc.importer.QuadLoader()
@@ -11,5 +8,3 @@ typedstuff = FILTER quads BY predicate == 'http://www.w3.org/1999/02/22-rdf-synt
 classes = GROUP typedstuff BY object.value;
 classCounts = FOREACH classes GENERATE group, COUNT(typedstuff) AS cnt;
 topClassesByEntities = ORDER classCounts BY cnt DESC;
-
-STORE topClassesByEntities INTO 'results-topClassesByEntities';
