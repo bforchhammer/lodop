9ea9a3a718785a36273a787ab82768fcb241fe14
Benedikt Forchhammer <b.forchhammer@mind2.de>
2013-10-01 14:55:00 +0200

    Fixed statistics display for failed jobs.

--- benchmark/src/main/java/de/uni_potsdam/hpi/loddp/benchmark/reporting/ExecutionStats.java
+++ benchmark/src/main/java/de/uni_potsdam/hpi/loddp/benchmark/reporting/ExecutionStats.java
@@ -139,54 +139,43 @@ public class ExecutionStats implements ScriptStats {
 
         sb.append("Total time: \t").append(DurationFormatUtils.formatDurationHMS(pigStats.getDuration())).append("\n");
 
-        if (pigStats.isSuccessful()) {
-            sb.append("JobId\tMaps\tReduces\tMaxMapTime\tMinMapTime\tAvgMapTime\tMaxReduceTime\t" +
-                "MinReduceTime\tAvgReduceTime\tAlias\tFeature\tOutputs\n");
-            List<JobStats> arr = pigStats.getJobGraph().getSuccessfulJobs();
-            for (JobStats js : arr) {
-                String id = (js.getJobId() == null) ? "N/A" : js.getJobId();
-                if (js.getState() == JobStats.JobState.FAILED) {
-                    sb.append(id).append("\t")
-                        .append(js.getAlias()).append("\t")
-                        .append(js.getFeature()).append("\t");
-                    if (js.getState() == JobStats.JobState.FAILED) {
-                        sb.append("Message: ").append(js.getErrorMessage()).append("\t");
-                    }
-                } else if (js.getState() == JobStats.JobState.SUCCESS) {
-                    sb.append(id).append("\t")
-                        .append(js.getNumberMaps()).append("\t")
-                        .append(js.getNumberReduces()).append("\t");
-                    if (js.getMaxMapTime() < 0) {
-                        sb.append("n/a\t").append("n/a\t").append("n/a\t");
-                    } else {
-                        sb.append(DurationFormatUtils.formatDurationHMS(js.getMaxMapTime())).append("\t")
-                            .append(DurationFormatUtils.formatDurationHMS(js.getMinMapTime())).append("\t")
-                            .append(DurationFormatUtils.formatDurationHMS(js.getAvgMapTime())).append("\t");
-                    }
-                    if (js.getMaxReduceTime() < 0) {
-                        sb.append("n/a\t").append("n/a\t").append("n/a\t");
-                    } else {
-                        sb.append(DurationFormatUtils.formatDurationHMS(js.getMaxReduceTime())).append("\t")
-                            .append(DurationFormatUtils.formatDurationHMS(js.getMinReduceTime())).append("\t")
-                            .append(DurationFormatUtils.formatDurationHMS(js.getAvgREduceTime())).append("\t");
-                    }
-                    sb.append(js.getAlias()).append("\t")
-                        .append(js.getFeature()).append("\t");
+        sb.append("JobId\tMaps\tReduces\tMaxMapTime\tMinMapTime\tAvgMapTime\tMaxReduceTime\t" +
+            "MinReduceTime\tAvgReduceTime\tAlias\tFeature\tOutputs\n");
+        List<JobStats> arr = pigStats.getJobGraph().getJobList();
+        for (JobStats js : arr) {
+            String id = (js.getJobId() == null) ? "N/A" : js.getJobId();
+            if (js.getState() == JobStats.JobState.FAILED) {
+                sb.append(id).append("\t")
+                    .append(js.getAlias()).append("\t")
+                    .append(js.getFeature()).append("\t")
+                    .append("Message: ").append(js.getErrorMessage()).append("\t");
+            } else if (js.getState() == JobStats.JobState.SUCCESS) {
+                sb.append(id).append("\t")
+                    .append(js.getNumberMaps()).append("\t")
+                    .append(js.getNumberReduces()).append("\t");
+                if (js.getMaxMapTime() < 0) {
+                    sb.append("n/a\t").append("n/a\t").append("n/a\t");
+                } else {
+                    sb.append(DurationFormatUtils.formatDurationHMS(js.getMaxMapTime())).append("\t")
+                        .append(DurationFormatUtils.formatDurationHMS(js.getMinMapTime())).append("\t")
+                        .append(DurationFormatUtils.formatDurationHMS(js.getAvgMapTime())).append("\t");
                 }
-                for (OutputStats os : js.getOutputs()) {
-                    sb.append(os.getLocation()).append(",");
+                if (js.getMaxReduceTime() < 0) {
+                    sb.append("n/a\t").append("n/a\t").append("n/a\t");
+                } else {
+                    sb.append(DurationFormatUtils.formatDurationHMS(js.getMaxReduceTime())).append("\t")
+                        .append(DurationFormatUtils.formatDurationHMS(js.getMinReduceTime())).append("\t")
+                        .append(DurationFormatUtils.formatDurationHMS(js.getAvgREduceTime())).append("\t");
                 }
-                sb.append("\n");
+                sb.append(js.getAlias()).append("\t")
+                    .append(js.getFeature()).append("\t");
             }
-        } else {
-            sb.append("Failed Jobs:\n");
-            sb.append(JobStats.FAILURE_HEADER).append("\n");
-            List<JobStats> arr = pigStats.getJobGraph().getFailedJobs();
-            for (JobStats js : arr) {
-                sb.append(js.toString());
+            for (OutputStats os : js.getOutputs()) {
+                sb.append(os.getLocation()).append(",");
             }
             sb.append("\n");
         }
+
 
         log.info(sb.toString());
 
